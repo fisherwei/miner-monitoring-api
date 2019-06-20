b80435fff729018630db4f2b0dd204bd4f40e18b
Fisher Pri Catz <fisher.wei@wqfw.de>
2019-06-20 14:24:18 +0800

    zennode

--- zennode.py
+++ zennode.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
 from flask_restful import reqparse, abort, Api, Resource
 import requests
+import os
 
 
 class API(Resource):
     def __init__(self):
         self.parser = reqparse.RequestParser()
         self.parser.add_argument('api', type=str)
-        self.parser.add_argument('key', type=str)
 
     def get(self, host):
         data = self.parser.parse_args()
         api = data.get('api')
-        key = data.get('key')
+        key = os.environ['ZEN_KEY']
 
         try:
             response = requests.get('https://securenodes2.na.zensystem.io{api}?key={key}'.format(api=api, key=key))
